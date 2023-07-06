249927da122ca3f92bda691fe812f60b77a25248
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-07-05 22:07:31 -0500

    added employer signup form

---
+++ components/Signup/SignupForm/EmployerSignup.js
@@ -0,0 +1,99 @@
+const EmployerSignup = ({ setEmployerInfo, employerInfo }) => {
+  const handleInput = (e, input) => {
+    switch (input) {
+      case "fName":
+        setEmployerInfo({ ...employerInfo, first: e.target.value });
+        break;
+      case "lName":
+        setEmployerInfo({ ...employerInfo, last: e.target.value });
+        break;
+      case "mid":
+        setEmployerInfo({ ...employerInfo, middle: e.target.value });
+        break;
+      case "city":
+        setEmployerInfo({ ...employerInfo, city: e.target.value });
+        break;
+      case "state":
+        setEmployerInfo({ ...employerInfo, state: e.target.value });
+        break;
+      case "zipcode":
+        setEmployerInfo({ ...employerInfo, zipcode: e.target.value });
+        break;
+      case "company":
+        setEmployerInfo({ ...employerInfo, company: e.target.value });
+        break;
+      case "address":
+        setEmployerInfo({ ...employerInfo, companyAddress: e.target.value });
+        break;
+      case "email":
+        setEmployerInfo({ ...employerInfo, companyEmail: e.target.value });
+        break;
+      case "phone":
+        setEmployerInfo({ ...employerInfo, companyPhone: e.target.value });
+        break;
+      default:
+        return;
+    }
+  };
+  return (
+    <>
+      <div>
+        <label htmlFor="fName">First: </label>
+        <input type="text" id="fName" onChange={(e) => handleInput(e, "fName")} value={employerInfo.first} />
+      </div>
+      <div>
+        <label htmlFor="mName">Middle: </label>
+        <input type="text" id="mName" onChange={(e) => handleInput(e, "mid")} value={employerInfo.middle} />
+      </div>
+      <div>
+        <label htmlFor="lName">Last: </label>
+        <input type="text" id="lName" onChange={(e) => handleInput(e, "lName")} value={employerInfo.last} />
+      </div>
+      <div>
+        <label htmlFor="city">City: </label>
+        <input type="text" id="city" onChange={(e) => handleInput(e, "city")} value={employerInfo.city} />
+      </div>
+      <div>
+        <label htmlFor="state">State: </label>
+        <input type="text" id="state" onChange={(e) => handleInput(e, "state")} value={employerInfo.state} />
+      </div>
+      <div>
+        <label htmlFor="zipcode">Zipcode: </label>
+        <input type="text" id="zipcode" onChange={(e) => handleInput(e, "zipcode")} value={employerInfo.zipcode} />
+      </div>
+      <div>
+        <label htmlFor="company">Company: </label>
+        <input type="text" id="company" onChange={(e) => handleInput(e, "company")} value={employerInfo.company} />
+      </div>
+      <div>
+        <label htmlFor="companyAddress">Company Address: </label>
+        <input
+          type="text"
+          id="companyAddress"
+          onChange={(e) => handleInput(e, "address")}
+          value={employerInfo.companyAddress}
+        />
+      </div>
+      <div>
+        <label htmlFor="companyEmail">Company Email: </label>
+        <input
+          type="text"
+          id="companyEmail"
+          onChange={(e) => handleInput(e, "email")}
+          value={employerInfo.companyEmail}
+        />
+      </div>
+      <div>
+        <label htmlFor="companyPhone">Company Phone: </label>
+        <input
+          type="text"
+          id="companyPhone"
+          onChange={(e) => handleInput(e, "phone")}
+          value={employerInfo.companyPhone}
+        />
+      </div>
+    </>
+  );
+};
+
+export default EmployerSignup;
--- components/Signup/SignupForm/index.js
+++ components/Signup/SignupForm/index.js
@@ -1,39 +1,69 @@
 import SeekerSignup from "./SeekerSignup";
+import EmployerSignup from "./EmployerSignup";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
 const SignupForm = () => {
-  const [employerInfo, setEmployerInfo] = useState({});
+  const router = useRouter();
+  const query = router.query.signupUser;
+  const [employerInfo, setEmployerInfo] = useState({
+    first: "",
+    middle: "",
+    last: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    company: "",
+    companyAddress: "",
+    companyEmail: "",
+    companyPhone: "",
+  });
   const [seekerInfo, setSeekerInfo] = useState({
     first: "",
     middle: "",
     last: "",
     city: "",
     state: "",
-    zipcode: 0,
+    zipcode: "",
     currentEmployer: "",
   });
-  const router = useRouter();
-  const query = router.query.signupUser;
 
   const signupFormHandler = (e) => {
     e.preventDefault();
-    console.log(seekerInfo);
-    setSeekerInfo({
-      first: "",
-      middle: "",
-      last: "",
-      city: "",
-      state: "",
-      zipcode: "",
-      currentEmployer: "",
-    });
+    console.log(employerInfo);
+    if (query === "seeker") {
+      setSeekerInfo({
+        first: "",
+        middle: "",
+        last: "",
+        city: "",
+        state: "",
+        zipcode: "",
+        currentEmployer: "",
+      });
+    }
+
+    if (query === "employer") {
+      setEmployerInfo({
+        first: "",
+        middle: "",
+        last: "",
+        city: "",
+        state: "",
+        zipcode: "",
+        company: "",
+        companyAddress: "",
+        companyEmail: "",
+        companyPhone: "",
+      });
+    }
   };
 
   return (
     <>
       <form onSubmit={signupFormHandler}>
         {query === "seeker" && <SeekerSignup setSeekerInfo={setSeekerInfo} seekerInfo={seekerInfo} />}
+        {query === "employer" && <EmployerSignup setEmployerInfo={setEmployerInfo} employerInfo={employerInfo} />}
         <button>Sign Up</button>
       </form>
     </>
