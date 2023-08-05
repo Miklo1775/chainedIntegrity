7ab402c80a39a819ddf18a90f850e4086110adb6
Victor "Miklo" Flores <94656623+Miklo1775@users.noreply.github.com>
2023-08-04 20:06:54 -0500

    added more details to single job card

--- components/Jobs/JobDisplay/SingleJobCard/index.js
+++ components/Jobs/JobDisplay/SingleJobCard/index.js
@@ -1,5 +1,14 @@
 const SingleJobCard = ({ job }) => {
-  return <div>{job.role}</div>;
+  return (
+    <div>
+      <img src={job.img} />
+      <h1>{job.name}</h1>
+      <h3>{job.role}</h3>
+      <p>
+        {job.locationCity},{job.locationState}
+      </p>
+    </div>
+  );
 };
 
 export default SingleJobCard;
